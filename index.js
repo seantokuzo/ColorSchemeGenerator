dfbd37171a199c0c27f44efc57db58bcdcd35936
CrackerJaps <thecrackerjaps@gmail.com>
2022-03-10 17:43:28 -0800

    Number of colors option implemented

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const toggler = document.getElementById('toggler')
 const colorPicker = document.getElementById('color-picker')
-const modeSelector = document.getElementById('mode-selector')
+const modeSelector = document.getElementById('mode-select')
+const numberSelector = document.getElementById('number-select')
 const getSchemeButton = document.getElementById('scheme-btn')
 const colorsDiv = document.getElementById('colors-div')
 let pickedColor = colorPicker.value.slice(1)
 let colorMode = modeSelector.value
+let numberColors = numberSelector.value
 let colorsMarkUp = ''
 
 function setLightMode() {
@@ -13,6 +15,9 @@ function setLightMode() {
   modeSelector.style.backgroundColor = 'var(--bg)'
   modeSelector.style.color = 'var(--text-light)'
   modeSelector.style.border = '2px solid var(--light-border)'
+  numberSelector.style.backgroundColor = 'var(--bg)'
+  numberSelector.style.color = 'var(--text-light)'
+  numberSelector.style.border = '2px solid var(--light-border)'
   getSchemeButton.style.backgroundColor = 'var(--bg)'
   getSchemeButton.style.color = 'var(--text-light)'
   getSchemeButton.style.border = '2px solid var(--light-border)'
@@ -24,6 +29,9 @@ function setDarkMode() {
   modeSelector.style.backgroundColor = 'var(--bg-dark)'
   modeSelector.style.color = 'var(--text-dark)'
   modeSelector.style.border = '2px solid var(--bg)'
+  numberSelector.style.backgroundColor = 'var(--bg-dark)'
+  numberSelector.style.color = 'var(--text-dark)'
+  numberSelector.style.border = '2px solid var(--bg)'
   getSchemeButton.style.backgroundColor = 'var(--btn-dark)'
   getSchemeButton.style.color = 'var(--text-dark)'
   getSchemeButton.style.border = 'none'
@@ -53,9 +61,15 @@ function getColorMode(event) {
 }
 modeSelector.addEventListener('change', getColorMode)
 
+function getNumberColors(event) {
+  numberColors = event.target.value
+  return numberColors
+}
+numberSelector.addEventListener('change', getNumberColors)
+
 getSchemeButton.addEventListener('click', function () {
   fetch(
-    `https://www.thecolorapi.com/scheme?hex=${pickedColor}&format=json&mode=${colorMode}&count=5`
+    `https://www.thecolorapi.com/scheme?hex=${pickedColor}&format=json&mode=${colorMode}&count=${numberColors}`
   )
     .then((res) => res.json())
     .then((data) => {
